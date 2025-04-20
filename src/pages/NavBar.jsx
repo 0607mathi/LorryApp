8becc8eed40d18b5a41bebe13933ae1631fc0630
0607mathi <mathiyarasu.m0607@gmail.com>
2025-04-20 17:26:14 +0530

    responsive nav bar completd

--- src/pages/NavBar.jsx
+++ src/pages/NavBar.jsx
@@ -1,21 +1,47 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { TiThMenu } from "react-icons/ti";
-const NavBar = () => {
-  return (
-    <div className='nav-bar'>
-      <Link to='/' className='logo'>Lorry Memo Book</Link>
-      <nav>
-      <Link to='/trip' className='link'>Trip</Link>
-      <Link className='link' to='/expense'>expense</Link>
-      <Link className='link' to='/diseal'>diseal</Link>
-      <Link className='link' to='/rto'>rto</Link>
-      <Link className='link' to='/smailage'>smailage</Link>
-      <Link className='link' to='/final'>final</Link>
-    </nav>
-    <button className='menubtn'><TiThMenu /></button>
-    </div>
-  )
-}
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { TiThMenu } from 'react-icons/ti';
 
-export default NavBar
+const NavBar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(prev => !prev);
+  };
+
+  return (
+    <>
+      <div className='nav-bar'>
+        <Link to='/' className='logo'>Lorry Memo Book</Link>
+        <nav className='desktop-nav'>
+          <Link to='/trip' className='link'>Trip</Link>
+          <Link className='link' to='/expense'>Expense</Link>
+          <Link className='link' to='/diseal'>Diesel</Link>
+          <Link className='link' to='/rto'>RTO</Link>
+          <Link className='link' to='/smailage'>Smailage</Link>
+          <Link className='link' to='/final'>Final</Link>
+        </nav>
+        <button className='menubtn' onClick={handleToggle}>
+          <TiThMenu />
+        </button>
+      </div>
+
+      {toggle && <Menu />}
+    </>
+  );
+};
+
+const Menu = () => {
+  return (
+    <nav className='menu'>
+      <Link to='/trip' className='link'>Trip</Link>
+      <Link className='link' to='/expense'>Expense</Link>
+      <Link className='link' to='/diseal'>Diesel</Link>
+      <Link className='link' to='/rto'>RTO</Link>
+      <Link className='link' to='/smailage'>Smailage</Link>
+      <Link className='link' to='/final'>Final</Link>
+    </nav>
+  );
+};
+
+export default NavBar;
